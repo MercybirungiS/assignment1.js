cf15c20323dce214c00a3652130a136bab602dd1
MercybirungiS <mercyssbirungi@gmail.com>
2021-03-09 17:43:27 +0300

    assignment1.js

---
+++ assignment1.js
@@ -0,0 +1,5 @@
+var fruits =["Avocadoes","Grapes","Mangoes","Orange","Kiwi"];
+//using forEach
+fruits.forEach(function(array){
+console.log(array);
+})
---
+++ classwork.js
@@ -0,0 +1,48 @@
+var age =18;
+/*if (age >=18){
+    console.log ("Go to school");
+    const newAge=age-10;
+    console.log(newAge);
+}
+else{
+    console.log("You are sort of an adult");
+}*/
+/*if (true){
+    let count = 78;
+    count=40;
+    console.log(count);
+}*/
+/*function run(){
+    var message ="Go to the forest";
+    if (true){
+        const friend ="Akinyi";
+        console.log(message);
+        console.log(friend);
+    }
+}
+run();*/
+
+let school="Akirachix";
+console.log("CodeHive"+school);
+
+var fruits =["Apples ", "Pineapples","Watermelon"]
+//using forEach
+fruits.forEach(element => {
+    console.log(element);
+
+});
+
+var children =20;
+function numbers(){
+    var girls = children-7;
+    console.log(girls);
+}
+numbers();
+var text ="outside";
+function logIt(){
+    var text = "inside";
+    console.log(text);
+}
+logIt();
+
+
